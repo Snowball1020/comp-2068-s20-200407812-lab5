c59bd2387a19e37d5c89ea45896e39814ef68a1f
Yuki <adohiito@gmail.com>
2020-06-15 17:53:51 -0400

    first commit

---
+++ controllers/SuperheroesController.js
@@ -0,0 +1,92 @@
+const viewPath = ('superheroes');
+const Superhero = require('../models/superhero');
+
+exports.index = async (req, res) => {
+  try {
+
+    const superheroes = await Superhero.find().sort({
+      updatedAt: "desc"
+    });
+    res.render(`${viewPath}/index`, {
+      superheroes: superheroes
+    })
+
+  } catch (error) {
+    req.flash('danger', 'There was an issue fetching the superheroes list');
+    res.redirect('/');
+  }
+}
+
+exports.show = async (req, res) => {
+  try {
+    const superhero = await Superhero.findById(req.params.id);
+
+    res.render(`${viewPath}/show`, {
+      pageTitle: '',
+      superhero: superhero
+    })
+  } catch (error) {
+    req.flash('danger', 'There was an issue fetching the superheroes list');
+    res.redirect('/');
+  }
+};
+
+exports.new = (req, res) => {
+  try {
+    res.render(`${viewPath}/new`, {
+      pageTitle: ''
+    })
+  } catch (error) {
+    req.flash('danger', 'There was an issue fetching the superheroes list');
+    res.redirect('/');
+  }
+};
+
+exports.create = async (req, res) => {
+  try {
+    const superhero = await Superhero.create(req.body);
+
+    req.flash('success', 'This hero was registered successfully');
+    res.redirect(`/superheroes/${superhero.id}`);
+  } catch (error) {
+    req.flash('danger', 'There was an issue fetching the superheroes list');
+    res.redirect('/');
+  }
+};
+
+exports.edit = async (req, res) => {
+  try {
+    const superhero = await Superhero.findById(req.params.id);
+
+    res.render(`${viewPath}/edit`, {
+      pageTitle: '',
+      formData: superhero
+    })
+  } catch (error) {
+    req.flash('danger', 'There was an issue fetching the superheroes list');
+    res.redirect('/');
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+
+
+    let hero = await Superhero.findById(req.body.id)
+
+    console.log(hero)
+
+    //    await Superhero.validate(req.body);
+    //    await Superhero.updateOne(req.body);
+    hero.name = await req.body.name;
+    hero.alias = await req.body.alias;
+
+    await hero.save()
+
+    req.flash('success', 'This hero was updated successfully');
+    res.redirect(`/superheroes/${req.body.id}`);
+  } catch (error) {
+    req.flash('danger', 'There was an issue fetching the superheroes list');
+    res.redirect('/');
+  }
+};
